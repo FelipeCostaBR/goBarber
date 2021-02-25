937b0f39e6ab87804d30f0e3675faab12f108d89
Felipe <felipejsilvacosta@gmail.com>
2021-02-25 22:20:09 +1100

    feature - notification/mongodb

--- src/modules/users/infra/http/routes/users.routes.ts
+++ src/modules/users/infra/http/routes/users.routes.ts
@@ -1,4 +1,6 @@
 import { Router } from 'express';
+import { celebrate, Segments, Joi } from 'celebrate';
+
 import multer from 'multer';
 
 import uploadConfig from '@config/upload';
@@ -13,7 +15,17 @@ const userController = new UsersController();
 const upload = multer(uploadConfig);
 const userAvatarController = new UsersAvatarController();
 
-usersRouter.post('/', userController.create);
+usersRouter.post(
+  '/',
+  celebrate({
+    [Segments.BODY]: {
+      name: Joi.string().required(),
+      email: Joi.string().email().required(),
+      password: Joi.string().required(),
+    },
+  }),
+  userController.create,
+);
 
 usersRouter.patch(
   '/avatar',
