a39a3122b64bf165be90505e2a2b72465416a034
Felipe <felipejsilvacosta@gmail.com>
2021-02-25 22:52:38 +1100

    add class-transformer library

--- src/modules/users/infra/http/controllers/SessionsController.ts
+++ src/modules/users/infra/http/controllers/SessionsController.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from 'express';
-
+import { classToClass } from 'class-transformer';
 import { container } from 'tsyringe';
 
 import AuthenticateUserService from '@modules/users/services/AuthenticateUserService';
@@ -14,8 +14,7 @@ export default class SessionsController {
       email,
       password,
     });
-    delete user.password;
 
-    return response.json({ user, token });
+    return response.json({ user: classToClass(user), token });
   }
 }
